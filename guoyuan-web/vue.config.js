6b05c957a308669f83d7598489b84ab780c78289
“boyboyyy” <1017850280qq.com>
2024-05-31 22:06:56 +0800

    强推

--- guoyuan-web/vue.config.js
+++ guoyuan-web/vue.config.js
@@ -1,6 +1,17 @@
 
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  devServer: {
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://47.96.226.103:8007',//要代理的本地api地址，也可以换成线上测试地址
+        changeOrigin: true,//允许跨域
+        pathRewrite: { "^/api": "/api" }//将/api开头替换为/api
+      }
+    }
+  },
+  lintOnSave: false,// 屏蔽EsLint
   transpileDependencies: true,
   lintOnSave: false
 });
